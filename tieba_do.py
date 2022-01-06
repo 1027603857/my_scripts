e9e0c0a41d56568b57c5af5534bb1cfc790e051a
1027603857 <38471268+1027603857@users.noreply.github.com>
2022-01-06 15:08:30 +0800

    Update tieba_do.py

--- tieba_do.py
+++ tieba_do.py
@@ -1,9 +1,19 @@
+// author: 疯疯
 /*
-贴吧签到
-=====================================
+幸运大转盘
+活动地址：https://pro.m.jd.com/mall/active/3ryu78eKuLyY5YipWWVSeRQEpLQP/index.html
+脚本兼容: Quantumult X, Surge, Loon, JSBox, Node.js
+==============Quantumult X==============
 [task_local]
-#贴吧签到
-*/5 * * * * tieba_do.py, tag=贴吧签到, enabled=true
+#幸运大转盘
+4 10 * * * https://gitee.com/lxk0301/jd_scripts/raw/master/jd_market_lottery.js, tag=幸运大转盘, enabled=true
+==============Loon==============
+[Script]
+cron "4 10 * * *" script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_market_lottery.js,tag=幸运大转盘
+================Surge===============
+幸运大转盘 = type=cron,cronexp="4 10 * * *",wake-system=1,timeout=3600,script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_market_lottery.js
+===============小火箭==========
+幸运大转盘 = type=cron,script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_market_lottery.js, cronexpr="4 10 * * *", timeout=3600, enable=true
 */
 
 import requests
