075e7506a8bb045bcee56b4136db77e9f2b5819f
1027603857 <38471268+1027603857@users.noreply.github.com>
2022-01-06 15:12:37 +0800

    Update tieba_do.py

--- tieba_do.py
+++ tieba_do.py
@@ -1,21 +1,11 @@
-// author: 疯疯
 /*
-幸运大转盘
-活动地址：https://pro.m.jd.com/mall/active/3ryu78eKuLyY5YipWWVSeRQEpLQP/index.html
-脚本兼容: Quantumult X, Surge, Loon, JSBox, Node.js
-==============Quantumult X==============
+贴吧签到
+====================================
 [task_local]
-#幸运大转盘
-4 10 * * * https://github.com/1027603857/my_scripts/edit/main/tieba_do.py, tag=幸运大转盘, enabled=true
-==============Loon==============
-[Script]
-cron "4 10 * * *" https://github.com/1027603857/my_scripts/edit/main/tieba_do.py,tag=幸运大转盘
-================Surge===============
-幸运大转盘 = type=cron,cronexp="4 10 * * *",wake-system=1,timeout=3600,https://github.com/1027603857/my_scripts/edit/main/tieba_do.py
-===============小火箭==========
-幸运大转盘 = type=cron,script-path=https://github.com/1027603857/my_scripts/edit/main/tieba_do.py, cronexpr="4 10 * * *", timeout=3600, enable=true
+#贴吧签到
+*/5 * * * * tieba_do.py, tag=贴吧签到, enabled=true
 */
-
+new Env("幸运大转盘");
 import requests
 url = "http://zlong.asia/TiebaSign/do.php"
 
