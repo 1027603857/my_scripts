4235bc9e20c9f2da60b954b1c971063ab0c3eb31
1027603857 <38471268+1027603857@users.noreply.github.com>
2022-01-07 13:31:11 +0800

    Update tieba_do.py

--- tieba_do.py
+++ tieba_do.py
@@ -8,9 +8,26 @@ new Env("贴吧签到");
 '''
 
 import requests
+import logging
+import os
+
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+logger = logging.getLogger(__name__)
+try:
+    import requests
+except Exception as e:
+    logger.info(str(e) + "\n\n缺少requests模块, 请执行命令：pip3 install requests\n\n")
+    sys.exit(1)
+os.environ['no_proxy'] = '*'
+requests.packages.urllib3.disable_warnings()
+try:
+    from notify import send
+except:
+    logger.info("无推送文件")
+
 url = "http://zlong.asia/TiebaSign/do.php"
 
 # 最简单的get请求
 r = requests.get(url)
-print(r.status_code)
-print(r.text)
+if (r.status_code != 200):
+    send("贴吧签到监控", "服务器访问错误，错误代码：%d"%r.status_code)
