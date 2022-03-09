fa48d884402d75215fe2aaaf1e127cf067486290
1027603857 <38471268+1027603857@users.noreply.github.com>
2022-03-10 01:14:23 +0800

    Update tieba_do.py

--- tieba_do.py
+++ tieba_do.py
@@ -3,7 +3,7 @@
 ====================================
 [task_local]
 #贴吧签到
-*/5 * * * * tieba_do.py, tag=贴吧签到, enabled=true
+*/5 0-2 * * * tieba_do.py, tag=贴吧签到, enabled=true
 new Env("贴吧签到");
 '''
 
