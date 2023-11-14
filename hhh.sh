4ce6ca8e6f7d10b7ec93d961c5bd51e7205289f3
1027603857 <38471268+1027603857@users.noreply.github.com>
2023-11-15 00:22:55 +0800

    Update hhh.sh

--- hhh.sh
+++ hhh.sh
@@ -47,7 +47,7 @@ check_url() {
 
 # 获取有效 config.sh 链接
 get_valid_config() {
-    config_list=(/https://raw.githubusercontent.com/yanyuwangluo/VIP/main/Conf/Qinglong/config.sample.sh)
+    config_list=(https://raw.githubusercontent.com/yanyuwangluo/VIP/main/Conf/Qinglong/config.sample.sh)
     for url in ${config_list[@]}; do
         check_url $url
         if [ $? = 0 ]; then
@@ -80,158 +80,7 @@ fi
 
 # 获取有效 extra.sh 链接
 get_valid_extra() {
-    extra_list=(/https://raw.githubusercontent.com/yanyuwangluo/VIP/main/Tasks/qlrepo/extra.sh)
-    for url in ${extra_list[@]}; do
-        check_url $url
-        if [ $? = 0 ]; then
-            valid_url=$url
-            echo "使用链接 $url"
-            break
-        fi
-    done
-}
-# 下载 extra.sh
-dl_extra_shell() {
-    if [ ! -a "$extra_shell_path" ]; then
-        touch $extra_shell_path
-    fi
-    curl -sL --connect-timeout 3 $valid_url > $extra_shell_path
-    cp $extra_shell_path $dir_config/extra.sh
-    # 判断是否下载成功
-    extra_size=$(ls -l $extra_shell_path | awk '{print $5}')
-    if (( $(echo "${extra_size} < 100" | bc -l) )); then
-        echo "extra.sh 下载失败"
-        exit 0
-    fi
-    # 授权
-    chmod 755 $extra_shell_path
-}
-# extra.sh 设置区设置
-set_default_extra() {   
-    echo -e "一、集成仓库 Shufflewzc-Faker2"
-    read -p "直接回车拉取Faker2仓库，输入2回车拉取Faker3纯净仓库" CollectedRepo
-    echo -e "二、其他仓库（Other Repositories）\n1-passerby-b\n2-curtinlv\n3-smiek2221\n4-cdle\n5-ZCY01\n6-whyour/hundun\n7-moposmall\n8-Ariszy (Zhiyi-N)\n9-photonmang\n10-jiulan\n12-star261\n13-Wenmoux\n14-Tsukasa007"
-    read -p "输入您想拉取其他仓库编号(回车默认不拉取)，拉取多个请用空格隔开:" OtherRepo 
-    CollectedRepo=${CollectedRepo:-"4"}
-    sed -i "s/CollectedRepo=(4)/CollectedRepo=(${CollectedRepo})/g" $extra_shell_path
-    sed -i "s/OtherRepo=()/OtherRepo=(${OtherRepo})/g" $extra_shell_path
-    echo -e "Ninja\n###（1）默认启动并自动更新\n###（2）！！！未修改容器映射的请勿运行，否则会出现青龙打不开或者设备死机等不良后果，映射参考 https://github.com/MoonBegonia/ninja#%E5%AE%B9%E5%99%A8%E5%86%85"
-    read -p "Ninja="on" ##up为更新，on为启动，down为不运行，请输入您的设置（默认运行） up/on/down：" Ninja
-    sed -i "s/Ninja=\"on\"/Ninja=\"${Ninja}\"/" $extra_shell_path
-}
-# 将 ql extra 添加到定时任务
-add_ql_extra() {
-    if [ "$(grep -c "ql\ extra" /ql/config/crontab.list)" != 0 ]; then
-        echo "您的任务列表中已存在 task:ql extra"
-    else
-        echo "开始添加 task:ql extra"
-        # 获取token
-        token=$(cat /ql/config/auth.json | jq --raw-output .token)
-        curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"初始化任务","command":"ql extra","schedule":"15 0-23/4 * * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1624782068473'
-    fi
-}
-# 运行一次 ql extra
-run_ql_extra() {
-    ql extra
-    sleep 5
-}
-if [ "${all}" = 1 ]; then
-    get_valid_extra && dl_extra_shell && set_default_extra && add_ql_extra && run_ql_extra
-elif [ "${extra}" = 'n' ]; then
-    echo "已为您跳过操作 extra.sh"
-else
-    if [[ ${extra} =~ 'a' ]]; then
-        get_valid_extra && dl_extra_shell
-    fi
-    if [[ ${extra} =~ 'b' ]]; then
-        set_default_extra
-    fi
-    if [[ ${extra} =~ 'c' ]]; then
-        add_ql_extra
-    fi
-    if [[ ${extra} =~ 'd' ]]; the#!/usr/bin/env bash
-
-## 本脚本搬运并模仿 liuqitoday
-dir_config=/ql/config
-dir_script=/ql/scripts
-dir_repo=/ql/repo
-config_shell_path=$dir_config/config.sh
-extra_shell_path=$dir_config/extra.sh
-code_shell_path=$dir_config/code.sh
-task_before_shell_path=$dir_config/task_before.sh
-bot_json=$dir_config/bot.json
-
-
-# 控制是否执行变量
-read -p "是否执行全部操作，输入 1 即可执行全部，输入 0 则跳出，回车默认和其他可进行选择性操作，建议初次配置输入 1：" all
-if [ "${all}" = 1 ]; then
-    echo "将执行全部操作"
-elif [ "${all}" = 0 ]; then
-    exit 0
-else
-    read -p "config.sh 操作（替换或下载选项为 y，不替换为 n，回车为替换）请输入：" Rconfig
-    Rconfig=${Rconfig:-'y'}
-    read -p "extra.sh 操作（替换或下载选项为 a，修改设置区设置为 b，添加到定时任务为 c，立即执行一次为 d，全部不执行为 n，回车全部执行 | 示例：acd）请输入：" extra
-    extra=${extra:-'abcd'}
-    read -p "code.sh 操作（替换或下载选项为 a，修改默认调用日志设置为 b，添加到定时任务为 c，全部不执行为 n，回车全部执行 | 示例：ac）请输入：" code
-    code=${code:-'abcd'}
-    read -p "task_before.sh 操作（替换或下载选项为 y，不替换为 n，回车为替换）请输入：" Rbefore
-    Rbefore=${Rbefore:-'y'}
-    read -p "已删除BOT设置"
-    #read -p "bot 操作（跳过为 0，添加 task:ql bot 选项为 1，添加后设置并运行为 2，回车等同 1）请输入:" bot
-    #bot=${bot:-'1'}
-    read -p "config.sample.sh 操作（跳过为 0，添加 task:自动更新模板 选项为 1，添加后运行一次为 2，回车等同 2）请输入：" sample
-    sample=${sample:-'2'}
-fi
-
-
-# 检查域名连通性
-check_url() {
-    HTTP_CODE=$(curl -o /dev/null --connect-timeout 3 -s -w "%{http_code}" $1)
-    if [ $HTTP_CODE -eq 200 ]; then
-        return 0
-    else
-        return 1
-    fi
-}
-
-
-# 获取有效 config.sh 链接
-get_valid_config() {
-    config_list=(/https://raw.githubusercontent.com/yanyuwangluo/VIP/main/Conf/Qinglong/config.sample.sh)
-    for url in ${config_list[@]}; do
-        check_url $url
-        if [ $? = 0 ]; then
-            valid_url=$url
-            echo "使用链接 $url"
-            break
-        fi
-    done
-}
-# 下载 config.sh
-dl_config_shell() {
-    if [ ! -a "$config_shell_path" ]; then
-        touch $config_shell_path
-    fi
-    curl -sL --connect-timeout 3 $valid_url > $config_shell_path
-    cp $config_shell_path $dir_config/config.sh
-    # 判断是否下载成功
-    config_size=$(ls -l $config_shell_path | awk '{print $5}')
-    if (( $(echo "${config_size} < 100" | bc -l) )); then
-        echo "config.sh 下载失败"
-        exit 0
-    fi
-}
-if [ "${Rconfig}" = 'y' -o "${all}" = 1 ]; then
-    get_valid_config && dl_config_shell
-else
-    echo "已为您跳过替换 config.sh"
-fi
-
-
-# 获取有效 extra.sh 链接
-get_valid_extra() {
-    extra_list=(/https://raw.githubusercontent.com/yanyuwangluo/VIP/main/Tasks/qlrepo/extra.sh)
+    extra_list=(https://raw.githubusercontent.com/yanyuwangluo/VIP/main/Tasks/qlrepo/extra.sh)
     for url in ${extra_list[@]}; do
         check_url $url
         if [ $? = 0 ]; then
@@ -308,7 +157,7 @@ fi
 
 # 获取有效 code.sh 链接
 get_valid_code() {
-    code_list=(/https://raw.githubusercontent.com/yanyuwangluo/VIP/main/Scripts/sh/Helpcode2.8/code.sh)
+    code_list=(https://raw.githubusercontent.com/yanyuwangluo/VIP/main/Scripts/sh/Helpcode2.8/code.sh)
     for url in ${code_list[@]}; do
         check_url $url
         if [ $? = 0 ]; then
@@ -374,7 +223,7 @@ fi
 
 # 获取有效 task_before.sh 链接
 get_valid_task_before() {
-    task_before_list=(/https://raw.githubusercontent.com/yanyuwangluo/VIP/main/Scripts/sh/Helpcode2.8/task_before.sh)
+    task_before_list=(https://raw.githubusercontent.com/yanyuwangluo/VIP/main/Scripts/sh/Helpcode2.8/task_before.sh)
     for url in ${task_before_list[@]}; do
         check_url $url
         if [ $? = 0 ]; then
@@ -459,7 +308,7 @@ add_curl_sample() {
         echo "开始添加 task:curl config.sample.sh"
         # 获取token
         token=$(cat /ql/config/auth.json | jq --raw-output .token)
-        curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"自动更新模板","command":"curl -L /https://raw.githubusercontent.com/yanyuwangluo/VIP/main/Conf/Qinglong/config.sample.sh -o /ql/sample/config.sample.sh && cp -rf /ql/sample/config.sample.sh /ql/config","schedule":"45 6,18 * * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1627380635389'
+        curl -s -H 'Accept: application/json' -H "Authorization: Bearer $token" -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept-Language: zh-CN,zh;q=0.9' --data-binary '{"name":"自动更新模板","command":"curl -L https://raw.githubusercontent.com/yanyuwangluo/VIP/main/Conf/Qinglong/config.sample.sh -o /ql/sample/config.sample.sh && cp -rf /ql/sample/config.sample.sh /ql/config","schedule":"45 6,18 * * *"}' --compressed 'http://127.0.0.1:5700/api/crons?t=1627380635389'
     fi
 }
 run_curl_sample() {
